4841763d061a63b5ff31cb4f34793741cc30e8c0
kalet chavez <kalet.svdev@gmail.com>
2026-04-15 23:38:55 -0500

    :sparkles: Agregar clase calculo

---
+++ main.py
@@ -0,0 +1,10 @@
+from services.vectors import VectorCalculated
+
+
+def run():
+    vector: VectorCalculated = VectorCalculated()
+
+
+
+if __name__ == "__main__":
+    run()
---
+++ services/vectors.py
@@ -0,0 +1,67 @@
+import math
+
+class VectorCalculated:
+
+    def __init__(self):
+        self.forceOneX: float = 0.0
+        self.forceOneY: float = 0.0
+        self.forceTwoX: float = 0.0
+        self.forceTwoY: float = 0.0
+        #componentes vector resultante
+        self.resultForceX: float = 0.0
+        self.resultForceY: float = 0.0
+
+        #componentes vector equlibrante
+        self.balancingForceX: float = 0.0
+        self.balancingForceY: float = 0.0
+    
+    def teoricVectorTwoForce(self, forceOne: float, thetaOne: float, 
+        forceTwo:float, thetaTwo: float ) -> object:
+        self.forceOneX = math.cos(thetaOne) * forceOne
+        self.forceOneY = math.sin(thetaOne) * forceOne
+
+        self.forceTwoX = math.cos(thetaTwo) * forceTwo
+        self.forceTwoY = math.sin(thetaTwo) * forceTwo
+
+        return { 
+            'forceOne': [self.forceOneX, self.forceOneY],
+            'forceTwo': [self.forceTwoX, self.forceTwoY]
+        }
+    
+    def resultForce(self):
+        self.resultForceX = self.forceOneX + self.forceTwoX
+        self.resultForceY = self.forceOneY + self.forceTwoY
+
+        return {
+            'resultForceX': self.resultForceX,
+            'resultForceY': self.resultForceY
+        }
+    
+    def resultNegativeForce(self) -> object:
+        self.balancingForceX = -self.resultForceX
+        self.balancingForceY = -self.resultForceY
+
+        return { 
+            'resultForceE->x': self.balancingForceX,
+            'resultForceE->y': self.balancingForceY
+        }
+    
+    def magnitudeForceResult(self) -> float:
+        sumary = ( self.resultForceX **2) + ( self.resultForceY**2)
+        magnitud = math.sqrt( sumary )
+
+        return magnitud
+    
+    def moduleResultForce(self) -> float:
+        resultForceModule = math.atan( (self.resultForceY/ self.resultForceX ) )
+        return resultForceModule
+    
+    def moduleBalancingForce( self ) -> float:
+        resultBalancingForce = math.atan( (self.balancingForceY/ self.balancingForceX) )
+        return resultBalancingForce
+    
+
+    
+    
+    
+
